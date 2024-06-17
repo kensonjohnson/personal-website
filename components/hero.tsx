afcc0ef1e174e6dadfbd5de5ae93fc955882c283
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-06-17 16:24:15 -0500

    Edit copy

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,16 +15,33 @@ export default function Hero() {
             alt="Me"
           />
           <h1 className="h1 mb-5 font-aspekta">
-            I write about coding and being a{" "}
-            <span className="relative inline-flex text-sky-500 before:absolute before:inset-0 before:-z-10 before:translate-y-1/4 before:-rotate-2 before:bg-sky-200 before:opacity-30 dark:before:bg-sky-500">
-              full-time
-            </span>{" "}
-            maker.
+            I write about{" "}
+            <span className="relative inline-flex text-sky-500 before:absolute before:inset-0 before:-z-10 before:translate-y-1 before:-rotate-2 before:bg-sky-200 before:opacity-30 dark:before:bg-sky-500">
+              code
+            </span>
+            .
           </h1>
           <p className="text-lg text-slate-500 dark:text-slate-400">
-            Writer, Speaker, Developer, and Co-Founder of Code.co, and
-            AppForYou. I write about coding, startups, and my journey as a
-            full-time maker.
+            Developer and Co-Founder of{" "}
+            <a
+              href="https://paperbullx.com"
+              className="relative text-white duration-150 ease-out before:absolute before:inset-0 before:-z-10 before:origin-left before:scale-x-0 before:bg-sky-200 before:opacity-30 before:duration-150 before:ease-in-out hover:before:scale-105 dark:before:bg-sky-500"
+              referrerPolicy="no-referrer"
+              target="_blank"
+            >
+              Paper Bull Trading
+            </a>
+            , and Co-Creator of{" "}
+            <a
+              href="https://spinespotter.com"
+              className="relative text-white duration-150 ease-out before:absolute before:inset-0 before:-z-10 before:origin-left before:scale-x-0 before:bg-sky-200 before:opacity-30 before:duration-150 before:ease-in-out hover:before:scale-105 dark:before:bg-sky-500"
+              referrerPolicy="no-referrer"
+              target="_blank"
+            >
+              Spine Spotter
+            </a>
+            . I write about coding, startups, and my journey as a full-time
+            maker.
           </p>
         </div>
       </div>
