9cc512640cd4ad61787b5d741187fa90324d9607
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-06-17 17:05:32 -0500

    Change styling on rendered MDX

--- components/mdx/mdx.tsx
+++ components/mdx/mdx.tsx
@@ -10,7 +10,7 @@ export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
   return (
-    <div className="prose max-w-none text-slate-500 prose-headings:text-slate-800 prose-p:leading-normal prose-a:font-medium prose-a:text-sky-500 prose-a:no-underline hover:prose-a:underline prose-strong:font-medium prose-strong:text-slate-800 prose-pre:bg-slate-800 dark:text-slate-400 dark:prose-headings:text-slate-200 dark:prose-strong:text-slate-100 dark:prose-code:text-slate-200">
+    <div className="prose max-w-none text-slate-600 prose-headings:text-slate-800 prose-p:leading-normal prose-a:font-medium prose-a:text-sky-500 prose-a:no-underline hover:prose-a:underline prose-strong:font-medium prose-strong:text-slate-800 prose-pre:bg-slate-800 dark:text-slate-100 dark:prose-headings:text-white dark:prose-strong:text-white dark:prose-code:text-white">
       <Component components={{ ...mdxComponents }} />
     </div>
   );
