6238e2d2dc58a758ec2e9ea4e65de48b9a740d8b
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-06-18 16:36:54 -0500

    Add contact route

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { ContactForm } from "@/components/Contact/ContactForm";
+
+export const metadata = {
+  title: "Contact - Kenson Johnson",
+  description: "Contact form for Kenson Johnson",
+};
+
+export default function Contact() {
+  return <ContactForm />;
+}
